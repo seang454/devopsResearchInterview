2a25a09aeb9eb2c96b7e2dcf6164869205f55055
seang454 <pengseangsim210@gmail.com>
2026-07-05 02:37:36 +0700

    update

--- terraform/infrastructure/live/dev/asia-southeast1/gcp-vm/main.tf
+++ terraform/infrastructure/live/dev/asia-southeast1/gcp-vm/main.tf
@@ -1,3 +1,5 @@
+// we will create file name backend.gcs.hcl to store the backend configuration for Terraform state in GCS bucket
+//syntax of backend.gcs.hcl will use name of backend.gcs , so it will know which .tf file to generate to main.tf
 terraform {
   # Supply bucket and prefix from backend.gcs.hcl during terraform init.
   backend "gcs" {}
