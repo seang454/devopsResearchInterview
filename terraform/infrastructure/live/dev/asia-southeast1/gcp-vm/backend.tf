9d4d9f4d113042e9b216f7ec1184825b895ed185
seang454 <pengseangsim210@gmail.com>
2026-07-06 13:07:42 +0700

    update

--- terraform/infrastructure/live/dev/asia-southeast1/gcp-vm/backend.tf
+++ terraform/infrastructure/live/dev/asia-southeast1/gcp-vm/backend.tf
@@ -2,6 +2,9 @@
 # Terraform reads it during:
 # terraform init -backend-config=backend.gcs.hcl
 # so the gernerated value will be stored in terraform.tfstate file not in this file.so backend "gcs"{} still the same as before, but the values are read from backend.gcs.hcl file.
+
 terraform {
+  # Supply bucket and prefix from backend.gcs.hcl during terraform init.
   backend "gcs" {}
 }
+#This means: This Terraform stack will use Google Cloud Storage to store its state.
